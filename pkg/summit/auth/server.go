8354c6a4fca45d7f2354729c9e5e919390a22dd7
Paal Gyula <paalgyula@paalgyula.com>
2023-11-12 15:39:16 +0100

    typo fix in error text

--- pkg/summit/auth/server.go
+++ pkg/summit/auth/server.go
@@ -22,7 +22,7 @@ import (
 var (
 	ErrShortRead = errors.New("short read when reading opcode data")
 	ErrWriteSize = errors.New("the written and sent bytes are not equal")
-	ErrNoHandler = errors.New("no handler implamented")
+	ErrNoHandler = errors.New("no handler implemented")
 )
 
 type Server struct {
